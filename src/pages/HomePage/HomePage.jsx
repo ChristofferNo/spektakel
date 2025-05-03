73f71a2d82792dd07e53d338018121fd683e2930
Christoffer Noren <christoffer.noren03@gmail.com>
2025-05-03 17:00:34 +0200

    frist step of homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,19 @@
 import "../HomePage/HomePage.css";
 
 
-function HomePage() {
+function HomePage({navigate}) {
   return (
     <div className="homePageContainer">
-      <div className="bandImage">Band</div>
+      <div className="hero-container">
+        <div className="dark-overlay">
+      <div className="hero-overlay">
+        <h1>Uppsalas främsta plattform för studentmusiker</h1>
+        <p>Vi för samman nationer, event organisatörer och musiker i Uppsala</p>
+        <button onClick={() => navigate('FindMusiciansPage')} className="hero-button">Hitta musiker</button>
+      </div>
+      </div>
+    </div>
+      
     </div>
   );
 }
