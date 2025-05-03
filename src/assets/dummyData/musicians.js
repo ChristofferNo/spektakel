11b88a2d614d579cc799dea878ceb2608693933d
Christoffer Noren <christoffer.noren03@gmail.com>
2025-05-03 19:22:57 +0200

    added centralized source for dummyData

---
+++ src/assets/dummyData/musicians.js
@@ -0,0 +1,43 @@
+import adriansBandImage from "../Images/placeHolderImage.jpeg";
+import BogdanBreakdance from "../Images/VinterBogdan.jpeg";
+import RubenOchTrubadurerna from "../Images/RubenTrub.jpeg";
+
+const musicians = [
+  {
+    name: "Adrians Band",
+    genres: ["Folk", "Acoustic"],
+    rating: 4.9,
+    price: 2200,
+    image: adriansBandImage,
+  },
+  {
+    name: "Bogdan Breakdance",
+    genres: ["Funk", "Jazz", "Coola"],
+    rating: 4.8,
+    price: 4500,
+    image: BogdanBreakdance,
+  },
+  {
+    name: "Ruben och Trubadurerna",
+    genres: ["Classical", "Chamber"],
+    rating: 4.8,
+    price: 4000,
+    image: RubenOchTrubadurerna,
+  },
+  {
+    name: "DJ Studentmix",
+    genres: ["Electronic", "Pop"],
+    rating: 4.7,
+    price: 3200,
+    image: "https://via.placeholder.com/300x200",
+  },
+  {
+    name: "Stand-up Students",
+    genres: ["Comedy", "Entertainment"],
+    rating: 4.6,
+    price: 3000,
+    image: "https://via.placeholder.com/300x200",
+  },
+];
+
+export default musicians;
--- src/pages/FindMusiciansPage/FindMusiciansPage.jsx
+++ src/pages/FindMusiciansPage/FindMusiciansPage.jsx
@@ -1,49 +1,9 @@
 import React, { useState } from "react";
 import "./FindMusiciansPage.css";
-import adriansBandImage from "../../assets/Images/placeHolderImage.jpeg";
-import BogdanBreakdance from "../../assets/Images/VinterBogdan.jpeg";
-import RubenOchTrubadurerna from "../../assets/Images/RubenTrub.jpeg";
+import musicians from "../../assets/dummyData/musicians";
 
 import MusicianCard from "../../components/MusicianCard/MusicianCard";
 
-
-const musicians = [
-  {
-    name: "Adrians Band",
-    genres: ["Folk", "Acoustic"],
-    rating: 4.9,
-    price: 2200,
-    image: adriansBandImage,
-  },
-  {
-    name: "Bogdan Breakdance",
-    genres: ["Funk", "Jazz", "Coola"],
-    rating: 4.8,
-    price: 4500,
-    image: BogdanBreakdance,
-  },
-  {
-    name: "Ruben och Trubadurerna",
-    genres: ["Classical", "Chamber"],
-    rating: 4.8,
-    price: 4000,
-    image: RubenOchTrubadurerna,
-  },
-  {
-    name: "DJ Studentmix",
-    genres: ["Electronic", "Pop"],
-    rating: 4.7,
-    price: 3200,
-    image: "https://via.placeholder.com/300x200",
-  },
-  {
-    name: "Stand-up Students",
-    genres: ["Comedy", "Entertainment"],
-    rating: 4.6,
-    price: 3000,
-    image: "https://via.placeholder.com/300x200",
-  },
-];
 
 function FindMusiciansPage({ navigate, setSelectedMusician }) {
   const [search, setSearch] = useState("");
