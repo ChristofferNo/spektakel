1e350d9d36d06d12d5d279247a8ddc6840e848b2
Christoffer Noren <christoffer.noren03@gmail.com>
2025-07-07 13:16:52 +0200

    om oss text

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -4,7 +4,13 @@ function AboutPage() {
   return (
     <div className="text-about">
       <h1>Om oss</h1>
-      <p>Vilka är vi?</p>
+      <p>
+        <h4>
+          Vi är tre studenter i Uppsala som vill se mer livemusik i vår stad och
+          vill göra det enklare för arrangörer och artister att hitta varandra.
+          <p>Med Spektakel vill vi stärka Uppsalas musikkultur.</p>
+        </h4>
+      </p>
     </div>
   );
 }
